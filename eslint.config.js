b6ea091f5d248f0448e5069c0aaaebc2ba9958a2
白雾茫茫丶 <baiwumm@foxmail.com>
2026-01-13 14:10:55 +0800

    feat: 删除项目没用的文件、配置项目开发所需要的配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js,ts,tsx,mjs}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +20,26 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "import/order": "off",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^node:"], // Node 内置模块
+            ["^@?\\w"], // 第三方模块
+            ["^@nestjs/"], // Nest.js 模块
+            ["^src/"], // 内部 alias
+            ["^\\."], // 相对路径
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      "no-duplicate-imports": "error",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
   },
-])
+]);
